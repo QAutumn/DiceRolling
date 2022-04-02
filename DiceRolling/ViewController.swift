b1aff8c7862add65efc6070c64befb4156a3e6f3
WenJie Qiu <qiuwenjie1997@163.com>
2022-04-02 22:49:22 +0800

    2

--- DiceRolling/ViewController.swift
+++ DiceRolling/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var a = 1
+    var b = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
